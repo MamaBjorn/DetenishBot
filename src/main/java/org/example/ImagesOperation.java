766b2f40d5f0203b2c50a984563b6dab9881df8a
MamaBjorn <3320135@gmail.com>
2023-09-30 19:05:11 +1000

    Initial commit

---
+++ src/main/java/org/example/ImagesOperation.java
@@ -0,0 +1,8 @@
+package org.example.functions;
+
+
+import java.lang.reflect.InvocationTargetException;
+
+public interface ImagesOperation {
+    float[] execute(float[] rgb) throws InvocationTargetException, IllegalAccessException;
+}
